7333d2b604b52e788c88324ec1f02fc9160e6848
MandaAnirudhReddy <245122748041@mvsrec.edu.in>
2025-03-24 12:51:45 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,14 @@
+class CommandLineDemo1
+{
+public static void main(String args[])
+{
+        String s1=args[0];
+        String s2=args[1];
+        String s3=args[2];
+
+System.out.println("Name:"+s1);
+System.out.println("rolno:"+s2);
+System.out.println("dept:"+s3);
+
+}
+}
